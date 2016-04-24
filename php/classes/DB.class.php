e50bebe60eb6939ebc1e0b834c2cac6d3abbad17
JakeTheMFSnake <jakob.hakansson@outlook.com>
2016-04-24 13:37:55 +0200

    Added DB class in php and updated vahhchat.php

---
+++ php/classes/DB.class.php
@@ -0,0 +1,47 @@
+<?php
+
+class DB {
+	private static $instance;
+	private $MySQLi;
+
+	/*Creates a connection to the database*/
+	private function __construct(array $dbOptions){
+		$this->MySQLi = @ new mysqli(
+		$dbOptions['db_host'],
+		$dbOptions['db_user'],
+		$dbOptions['db_pass'],
+		$dbOptions['db_name']
+		);
+		if(mysqli_connect_errno()){
+			throw new Exception('Databse error.');
+		}
+		$this->MySQLi->set_charset("utf8");
+	}
+
+	/*Takes an array with MySQL login details,
+	and creates an instance of the class,
+	held in the self::$instance static variable.
+	This way we can be sure that only one connection
+	to the database can exists in the same time*/
+	public static function init(array $dbOptions){
+		if(self::$instance instanceof self){
+			return false;
+		}
+		self::$instance = new self($dbOptions);
+	}
+
+	/*Retrives the SQLi object*/
+	public static function getMySQLiObject(){
+		return self::$instance->MySQLi;
+	}
+	/*Returns the result of the sql query*/
+	public static function query($sql){
+		return self::$instance-->MySQLi->query($sql);
+	}
+
+	/**/
+	public static function esc($str){
+		return self::$instance->MySQLi->real_escape_string(htmlspecialchars($str));
+	}
+
+?>
--- php/vahhchat.php
+++ php/vahhchat.php
@@ -11,6 +11,51 @@ jQuery front end and outputs JSON formatted data.*/
 sesson_name('webchat');
 session_start();
 
+/*Gets the current configuration setting of magic_quotes_gpc*/
+/*Returns 0 if magic_quotes_gpc is off, 1 otherwise. Or always
+returns FALSE as of PHP 5.4.0.*/
+if(get_magic_quotes_gpc()){
 
+    // If magic quotes is enabled, strip the extra slashes
+    array_walk_recursive($_GET,create_function('&$v,$k','$v = stripslashes($v);'));
+    array_walk_recursive($_POST,create_function('&$v,$k','$v = stripslashes($v);'));
+}
+
+try {
+  // Connecting to the database
+  DB::init($dbOptions);
+
+  $response = array();
+
+  // Handling the supported actions:
+  switch($_GET['action']){
+    case 'login':
+      
+    break;
+
+    case 'checkLogged':
+
+    break;
+
+    case 'logout':
+
+    break;
+
+    case 'submitChat':
+
+    break;
+
+    case 'getUser':
+
+    break;
+
+    case 'getChats':
+
+    break;
+
+    default:
+      throw new Exception('Wrong action');
+  }
+}
 
 ?>
